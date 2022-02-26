48d79a51cfaea84d3fd89ae1a835c34c988084f5
Sebastian Aguero <sebasaguero.06@gmail.com>
2022-02-26 01:19:05 -0300

    Inputs validations

--- import_info.php
+++ import_info.php
@@ -1,10 +1,10 @@
 <?php
 
-ob_start();
+ob_start(); 
 include ("./index.php");
-ob_end_clean();
+ob_end_clean(); //INCHARGE TO NOT SHOW THE INCLUDED HTML FROM INDEX.PHP
 
-// get the post records
+// GETS ALL INPUTS
 $uemail = $_POST['uemail'];
 $uname = $_POST['uname'];
 $ulname = $_POST['ulname'];
@@ -14,11 +14,10 @@ $urecomendacion = $_POST['urecomendacion'];
 // database insert SQL code
 $sql = "INSERT INTO `userdb` (uemail,uname,ulname,ufeedback,urecomendacion) VALUES ('$uemail', '$uname', '$ulname', '$ufeedback', '$urecomendacion')";
 
-// echo $sql 
-
 // insert in database 
 $rs = mysqli_query($con, $sql);
 
+// FUNCTION THAT VALIDATES THE EMAIL PLACED ON THE INPUTS AREN'T USED ALREADY
 if($rs)
 {
 	;
@@ -27,7 +26,58 @@ if($rs)
     echo "El correo utilizado ya envió un feedback, favor utilizar otro correo", exit;
 }
 
+// CHART
+
+if(isset($_POST["action"]))
+{
+	if($_POST["action"] == 'insert')
+	{
+		$data = array(
+			':ufeedback'		=>	$_POST["ufeedback"]
+		);
+
+		$query = "
+		INSERT INTO userdb 
+		(ufeedback) VALUES (:ufeedback)
+		";
+
+		$statement = $connect->prepare($query);
+
+		$statement->execute($data);
+
+		echo 'done';
+	}
+
+	if($_POST["action"] == 'fetch')
+	{
+		$query = "
+		SELECT ufeedback, COUNT(id) AS Total 
+		FROM userdb 
+		GROUP BY ufeedback
+		";
+
+		$result = $connect->query($query);
+
+		$data = array();
+
+		foreach($result as $row)
+		{
+			$data[] = array(
+				'ufeedback'		=>	$row["ufeedback"],
+				'total'			=>	$row["Total"],
+				'color'			=>	'#' . rand(100000, 999999) . ''
+			);
+		}
+
+		echo json_encode($data);
+	}
+}
+
+
+
 ?>
+
+<!-- IMPORT INFOR HTML -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,13 +85,17 @@ if($rs)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdnjs.com/libraries/Chart.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+   
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
     <title>Document</title>
 </head>
 <body>
 
-    <div class="card card-header text-center"> <h1>Gracias por completar el formulario <?php echo $uname, " ",$ulname?> </h1> </div>
+    <div class="card card-header text-center card bg-primary text-white"> <h1>Gracias por completar el formulario <?php echo $uname, " ",$ulname?> </h1> </div>
 
     <div class="card card-body">
         <h3>Acontinuación podrá ver la lista de sus datos enviados</h3>
@@ -52,5 +106,29 @@ if($rs)
         <p>Recomendación:<?php echo ' ', $urecomendacion?></p>
     </div>
 
+    <div class="container-fluid">
+        <div class="col-md-4">
+            <div class="card mt-4">
+                <div class="card-header">Grafico</div>
+                <div class="card-body">
+                    <div class="chart-container pie-chart">
+                        <canvas id="pie_chart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="card" id="card-1">
+        <div class="chart-canvas">
+            <canvas id="db_chart"></canvas>
+            <script>
+                var Pie = document.getElementById('ExpensesByCategory').getContext('2d');
+                PieChart = new Chart(Pie,{ type: 'pie',data: DataEC, option: optionsEC});
+            </script>
+        </div>
+    </div> -->
+    <script type="text/javascript" src="utility.js"></script>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -7,55 +7,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     
-    <script src="https://cdnjs.com/libraries/Chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
     <link rel="stylesheet" href="estilos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 </head>
-<body onload='document.form1.text1.focus()'>
+<body>
     <section class="section-container">
        <div class="content-wrapper">
-            <div class="contenido" id="primario"></div>
-            <!-- <div class="content-heading">FORMULARIO</div> -->
+            <!-- FORM -->
+            <form method="post" action="./import_info.php">
 
-            <form method="post" action="./import_info.php" action="#" name="form1">
-                <div class="card card-header text-center"> <h1>Completa el siguiente formulario de satisfacción</h1> </div>
+                <!-- FORM HEAD -->
+                <div class="card card-header text-center bg-primary text-white"> <h1>Completa el siguiente formulario de satisfacción</h1> </div>
+                
+                <!-- FORM BODDY -->
                 <div class="card card-body"> 
 
                     <div class="" class="">
+
+                        <!-- INPUT EMAIL -->
                         <div class="form-group col-md-3" class="mail">
                             <label>Email</label>
-                            <input class="form-control" type="text" value="" name="uemail" id="uemail" data-parsley-secundario="">
+                            <input class="form-control" value="" name="uemail" id="uemail" placeholder="ejemplo@gmail.com" type="email" required>
                         </div>
 
+                        <!-- INPUT NOMBRE -->
                         <div class="form-group col-md-3">
                             <label>Nombre</label>
-                            <input class="form-control" type="text" value="" name="uname" id="uname" data-parsley-secundario="">
+                            <input class="form-control" type="text" value="" name="uname" id="uname" placeholder="Escribe su nombre aquí" required>
                         </div>
 
+                        <!-- INPUT APELLIDO -->
                         <div class="form-group col-md-3">
                             <label>Apellido</label>
-                            <input class="form-control" type="text" value="" name="ulname" id="ulname" data-parsley-secundario="">
+                            <input class="form-control" value="" name="ulname" id="ulname" type="text" placeholder="Escribe su apellido aquí" required>
                         </div>
 
+                        <!-- INPUT PUNTUACION -->
                         <div class="form-group col-md-3" class="Points">
                             <label>Puntuacion</label>
-                            <select onchange="displayDiv('contactme',this)" class="form-control lista lista_simple" data-parsley-notblank="true" name="ufeedback" id="ufeedback" data-parsley-errors-container="#feedback_error" required="" aria-placeholder="">                         
-                            <option value=" " ></option><option value="1 - Insuficiente">1 - Insuficiente</option><option value="2 - Pasable">2 - Pasable</option><option value="3 - Pasable">3 - Bueno</option><option value="4 - Pasable">4 - Muy Bueno</option><option value="5 - Excelente">5 - Excelente</option>
+                            <select onchange="displayDiv('contactme',this)" class="form-control lista lista_simple" name="ufeedback" id="ufeedback" required>                         
+                                <option value="">Selecciona una puntuación</option>
+                                <option value="1 - Insuficiente">1 - Insuficiente</option>
+                                <option value="2 - Pasable">2 - Pasable</option>
+                                <option value="3 - Pasable">3 - Bueno</option>
+                                <option value="4 - Pasable">4 - Muy Bueno</option>
+                                <option value="5 - Excelente">5 - Excelente</option>
                             </select>
                             
+                            <!-- HIDDEN DIV - WORKS WITH A CONDITION -->
                             <div id="contactme" >
                                 <label>Por favor, expliquenos el motivo por el cual usted selecciono una puntuación baja comunicate con nosotros: +595 XXX XXX XXX </label>
                             </div>
+
                         </div>
 
+                        <!-- INPUT RECOMENDACION -->
                         <div class="form-group col-md-3">
                             <label>Recomendación</label>
-                            <textarea class="form-control" type="text" value="" name="urecomendacion" id="urecomendacion" style="height: 100px"></textarea>
-                            <!-- <input class="form-control" type="text" value="" name="urecomendacion" id="urecomendacion" data-parsley-secundario=""> -->
+                            <textarea class="form-control" type="text" value="" name="urecomendacion" id="urecomendacion" style="height: 100px" placeholder="Aquí podrá presentar sus quejas y recomendaciones"></textarea>
                         </div>
                         
+                        <!-- SAVE BUTTON -->
                         <div class="col-xl text-center" class="target">
                             <button class="btn btn-labeled btn-primary mb-2" id="submit" type="submit"><span class="btn-label"><i class="fa fa-save"></i></span> Enviar </button>                        
                         </div>
@@ -66,10 +81,13 @@
        </div> 
     </section>
 
+    <!-- INCLUDE INDEX.JS SCRIPTS -->
     <script src="index.js"></script>
 
 </body>
 </html>
+
+<!-- SQL CONNECTION -->
 
 <?php
 
@@ -79,3 +97,4 @@ $con = new mysqli('localhost', 'root', '','formdatabase');
 
 
 ?>
+
